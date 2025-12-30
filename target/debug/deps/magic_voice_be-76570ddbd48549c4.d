d75e035f57b77d9e6f7acd6bd38fdc8d7a27f6b5
Kamil Gaborek <kamil.gaborek@telenorlinx.com>
2025-12-30 14:05:54 +0100

    init commit

---
+++ target/debug/deps/magic_voice_be-76570ddbd48549c4.d
@@ -0,0 +1,5 @@
+/Users/kamil.gaborek/repos/private/magicvoice_saas/magic_voice_be/target/debug/deps/magic_voice_be-76570ddbd48549c4.d: src/main.rs
+
+/Users/kamil.gaborek/repos/private/magicvoice_saas/magic_voice_be/target/debug/deps/libmagic_voice_be-76570ddbd48549c4.rmeta: src/main.rs
+
+src/main.rs:

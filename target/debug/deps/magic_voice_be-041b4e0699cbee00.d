b6f1d357a99efe783382dc71dd22fcd080b0326b
Kamil Gaborek <kamil.gaborek@gmail.com>
2026-01-02 15:43:50 +0100

    clean up remote

--- target/debug/deps/magic_voice_be-041b4e0699cbee00.d
+++
@@ -1,5 +0,0 @@
-/Users/kamil.gaborek/repos/private/magicvoice_saas/magic_voice_be/target/debug/deps/magic_voice_be-041b4e0699cbee00.d: src/main.rs
-
-/Users/kamil.gaborek/repos/private/magicvoice_saas/magic_voice_be/target/debug/deps/libmagic_voice_be-041b4e0699cbee00.rmeta: src/main.rs
-
-src/main.rs:
